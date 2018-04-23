fd7b7668441f83cae3f2e9df83287dc92e6cb32d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using DevExpress.XtraEditors.Controls;
+using DevExpress.XtraEditors.Repository;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // create images for "Hatch Styles" ImageComboBoxEdit            
+            CreateHatchStyleItems();
+
+
+            // create images for "Custom Colors" ImageComboBoxEdit
+            CreateCustomColorsItems();
+
+            // in-place editing
+            CreatedGridControlSource();
+
+        }
+
+        private void CreatedGridControlSource()
+        {
+            BindingList<Test> source = new BindingList<Test>();
+            source.Add(new Test() { Style = HatchStyle.DarkDownwardDiagonal, CustomColor = Color.Red });
+            source.Add(new Test() { Style = HatchStyle.DarkVertical, CustomColor = Color.Yellow });
+            source.Add(new Test() { Style = HatchStyle.Percent10, CustomColor = Color.Brown });
+            source.Add(new Test() { Style = HatchStyle.Trellis, CustomColor = Color.White });
+
+            gridControl1.DataSource = source;
+            gridControl1.ForceInitialize();
+
+            RepositoryItemImageComboBox reHS = gridControl1.RepositoryItems.Add("ImageComboBoxEdit") as RepositoryItemImageComboBox;
+            reHS.Assign(imageComboBoxEdit1.Properties);
+            gridView1.Columns["Style"].ColumnEdit = reHS;
+
+
+            RepositoryItemImageComboBox reColors = gridControl1.RepositoryItems.Add("ImageComboBoxEdit") as RepositoryItemImageComboBox;
+            reColors.Assign(imageComboBoxEdit2.Properties);
+            gridView1.Columns["CustomColor"].ColumnEdit = reColors;
+        }
+
+        private void CreateCustomColorsItems()
+        {
+            List<Color> customColors = new List<Color>();
+            customColors.AddRange(new Color[] { Color.Red, Color.Black, Color.Yellow, Color.Green, Color.Blue, Color.Brown, Color.White });
+            foreach (Color color in customColors)
+            {
+                imageComboBoxEdit2.Properties.Items.Add(new ImageComboBoxItem(color.ToString(), color));
+            }
+
+            ImageList imagesColors = new ImageList();
+            imageComboBoxEdit2.Properties.SmallImages = imagesColors;
+
+            foreach (ImageComboBoxItem item in imageComboBoxEdit2.Properties.Items)
+            {
+                int iWidth = 16;
+                int iHeight = 16;
+                Bitmap bmp = new Bitmap(iWidth, iHeight);
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.DrawRectangle(new Pen(Color.Black, 2), 0, 0, iWidth, iHeight);
+                    g.FillRectangle(new SolidBrush((Color)item.Value), 1, 1, iWidth - 2, iHeight - 2);
+
+                }
+                imagesColors.Images.Add(bmp);
+                item.ImageIndex = imagesColors.Images.Count - 1;
+            }
+        }
+
+        private void CreateHatchStyleItems()
+        {
+            imageComboBoxEdit1.Properties.Items.AddEnum(typeof(System.Drawing.Drawing2D.HatchStyle));
+
+            ImageList imagesHS = new ImageList();
+            imageComboBoxEdit1.Properties.SmallImages = imagesHS;
+
+            foreach (ImageComboBoxItem item in imageComboBoxEdit1.Properties.Items)
+            {
+                int iWidth = 16;
+                int iHeight = 16;
+                Bitmap bmp = new Bitmap(iWidth, iHeight);
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.DrawRectangle(new Pen(Color.Black, 2), 0, 0, iWidth, iHeight);
+                    HatchBrush b = new HatchBrush((HatchStyle)item.Value, Color.Black, Color.White);
+                    g.FillRectangle(b, 0, 0, iWidth, iHeight);
+
+                }
+                imagesHS.Images.Add(bmp);
+                item.ImageIndex = imagesHS.Images.Count - 1;
+            }
+        }
+    }
+
+    public class Test
+    {
+        public HatchStyle Style { get; set; }
+        public Color CustomColor { get; set; }
+    }
+}
---
+++ VB/WindowsFormsApplication1/Form1.Designer.vb
@@ -0,0 +1,177 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Namespace WindowsFormsApplication1
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.layoutControl1 = New DevExpress.XtraLayout.LayoutControl()
+			Me.layoutControlGroup1 = New DevExpress.XtraLayout.LayoutControlGroup()
+			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+			Me.layoutControlItem1 = New DevExpress.XtraLayout.LayoutControlItem()
+			Me.imageComboBoxEdit1 = New DevExpress.XtraEditors.ImageComboBoxEdit()
+			Me.layoutControlItem2 = New DevExpress.XtraLayout.LayoutControlItem()
+			Me.imageComboBoxEdit2 = New DevExpress.XtraEditors.ImageComboBoxEdit()
+			Me.layoutControlItem3 = New DevExpress.XtraLayout.LayoutControlItem()
+			CType(Me.layoutControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.layoutControl1.SuspendLayout()
+			CType(Me.layoutControlGroup1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.layoutControlItem1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.imageComboBoxEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.layoutControlItem2, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.imageComboBoxEdit2.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.layoutControlItem3, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' layoutControl1
+			' 
+			Me.layoutControl1.Controls.Add(Me.imageComboBoxEdit2)
+			Me.layoutControl1.Controls.Add(Me.imageComboBoxEdit1)
+			Me.layoutControl1.Controls.Add(Me.gridControl1)
+			Me.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.layoutControl1.Location = New System.Drawing.Point(0, 0)
+			Me.layoutControl1.Name = "layoutControl1"
+			Me.layoutControl1.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = New System.Drawing.Rectangle(599, 260, 671, 545)
+			Me.layoutControl1.Root = Me.layoutControlGroup1
+			Me.layoutControl1.Size = New System.Drawing.Size(549, 416)
+			Me.layoutControl1.TabIndex = 0
+			Me.layoutControl1.Text = "layoutControl1"
+			' 
+			' layoutControlGroup1
+			' 
+			Me.layoutControlGroup1.CustomizationFormText = "layoutControlGroup1"
+			Me.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True
+			Me.layoutControlGroup1.GroupBordersVisible = False
+			Me.layoutControlGroup1.Items.AddRange(New DevExpress.XtraLayout.BaseLayoutItem() { Me.layoutControlItem2, Me.layoutControlItem1, Me.layoutControlItem3})
+			Me.layoutControlGroup1.Location = New System.Drawing.Point(0, 0)
+			Me.layoutControlGroup1.Name = "layoutControlGroup1"
+			Me.layoutControlGroup1.Size = New System.Drawing.Size(549, 416)
+			Me.layoutControlGroup1.Text = "layoutControlGroup1"
+			Me.layoutControlGroup1.TextVisible = False
+			' 
+			' gridControl1
+			' 
+			Me.gridControl1.Location = New System.Drawing.Point(12, 52)
+			Me.gridControl1.MainView = Me.gridView1
+			Me.gridControl1.Name = "gridControl1"
+			Me.gridControl1.Size = New System.Drawing.Size(525, 352)
+			Me.gridControl1.TabIndex = 4
+			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
+			' 
+			' gridView1
+			' 
+			Me.gridView1.GridControl = Me.gridControl1
+			Me.gridView1.Name = "gridView1"
+			' 
+			' layoutControlItem1
+			' 
+			Me.layoutControlItem1.Control = Me.gridControl1
+			Me.layoutControlItem1.CustomizationFormText = "layoutControlItem1"
+			Me.layoutControlItem1.Location = New System.Drawing.Point(0, 40)
+			Me.layoutControlItem1.Name = "layoutControlItem1"
+			Me.layoutControlItem1.Size = New System.Drawing.Size(529, 356)
+			Me.layoutControlItem1.Text = "layoutControlItem1"
+			Me.layoutControlItem1.TextSize = New System.Drawing.Size(0, 0)
+			Me.layoutControlItem1.TextToControlDistance = 0
+			Me.layoutControlItem1.TextVisible = False
+			' 
+			' imageComboBoxEdit1
+			' 
+			Me.imageComboBoxEdit1.Location = New System.Drawing.Point(12, 28)
+			Me.imageComboBoxEdit1.Name = "imageComboBoxEdit1"
+			Me.imageComboBoxEdit1.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() { New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+			Me.imageComboBoxEdit1.Size = New System.Drawing.Size(260, 20)
+			Me.imageComboBoxEdit1.StyleController = Me.layoutControl1
+			Me.imageComboBoxEdit1.TabIndex = 5
+			' 
+			' layoutControlItem2
+			' 
+			Me.layoutControlItem2.Control = Me.imageComboBoxEdit1
+			Me.layoutControlItem2.CustomizationFormText = "Hatch Styles"
+			Me.layoutControlItem2.Location = New System.Drawing.Point(0, 0)
+			Me.layoutControlItem2.Name = "layoutControlItem2"
+			Me.layoutControlItem2.Size = New System.Drawing.Size(264, 40)
+			Me.layoutControlItem2.Text = "Hatch Styles"
+			Me.layoutControlItem2.TextLocation = DevExpress.Utils.Locations.Top
+			Me.layoutControlItem2.TextSize = New System.Drawing.Size(67, 13)
+			' 
+			' imageComboBoxEdit2
+			' 
+			Me.imageComboBoxEdit2.Location = New System.Drawing.Point(276, 28)
+			Me.imageComboBoxEdit2.Name = "imageComboBoxEdit2"
+			Me.imageComboBoxEdit2.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() { New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+			Me.imageComboBoxEdit2.Size = New System.Drawing.Size(261, 20)
+			Me.imageComboBoxEdit2.StyleController = Me.layoutControl1
+			Me.imageComboBoxEdit2.TabIndex = 6
+			' 
+			' layoutControlItem3
+			' 
+			Me.layoutControlItem3.Control = Me.imageComboBoxEdit2
+			Me.layoutControlItem3.CustomizationFormText = "Custom colors"
+			Me.layoutControlItem3.Location = New System.Drawing.Point(264, 0)
+			Me.layoutControlItem3.Name = "layoutControlItem3"
+			Me.layoutControlItem3.Size = New System.Drawing.Size(265, 40)
+			Me.layoutControlItem3.Text = "Custom colors"
+			Me.layoutControlItem3.TextLocation = DevExpress.Utils.Locations.Top
+			Me.layoutControlItem3.TextSize = New System.Drawing.Size(67, 13)
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(549, 416)
+			Me.Controls.Add(Me.layoutControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+'			Me.Load += New System.EventHandler(Me.Form1_Load);
+			CType(Me.layoutControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.layoutControl1.ResumeLayout(False)
+			CType(Me.layoutControlGroup1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.layoutControlItem1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.imageComboBoxEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.layoutControlItem2, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.imageComboBoxEdit2.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.layoutControlItem3, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private layoutControl1 As DevExpress.XtraLayout.LayoutControl
+		Private imageComboBoxEdit2 As DevExpress.XtraEditors.ImageComboBoxEdit
+		Private imageComboBoxEdit1 As DevExpress.XtraEditors.ImageComboBoxEdit
+		Private gridControl1 As DevExpress.XtraGrid.GridControl
+		Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+		Private layoutControlGroup1 As DevExpress.XtraLayout.LayoutControlGroup
+		Private layoutControlItem2 As DevExpress.XtraLayout.LayoutControlItem
+		Private layoutControlItem1 As DevExpress.XtraLayout.LayoutControlItem
+		Private layoutControlItem3 As DevExpress.XtraLayout.LayoutControlItem
+	End Class
+End Namespace
+
